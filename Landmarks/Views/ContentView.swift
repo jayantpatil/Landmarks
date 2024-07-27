63c85f1123dd6af1222c7be5d1ae6e77d2931938
Jayant <jayant.ptl@gmail.com>
2024-07-28 01:51:16 +0530

    Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jayant D Patil on 27/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
