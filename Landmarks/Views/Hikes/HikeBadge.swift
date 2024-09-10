d7687ecc4ad095d4836b517ca64f5ab63d8fb26f
Jayant <jayant.ptl@gmail.com>
2024-09-10 23:34:09 +0530

    App design and layout Working with UI controls

---
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,29 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by Jayant D Patil on 10/09/24.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0/3.0)
+                .frame(width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+            
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name).")
+        }
+    }
+}
+
+#Preview {
+    HikeBadge(name: "Preview Testing")
+}
